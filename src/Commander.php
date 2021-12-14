9d2f1e775883fa7bb5052f35f9e931942e00b2df
Shazahanul Islam Shohag <shohag121@gmail.com>
2021-12-14 21:01:39 +0600

    update: `Commander` Introduces

---
+++ src/Commander.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WPDevICU\WPPG;
+
+use Symfony\Component\Console\Application;
+use WPDevICU\WPPG\Commands\GenerateCommand;
+use WPDevICU\WPPG\Commands\NewCommand;
+
+class Commander
+{
+    public const VERSION = '0.0.1';
+
+    protected Application $application;
+
+    public function __construct()
+    {
+        $this->application = new Application('WordPress Boilerplate Generator', self::VERSION);
+        $this->application->add(new NewCommand());
+        $this->application->add(new GenerateCommand());
+    }
+
+    public function lead(): void
+    {
+        $this->application->run();
+    }
+}
